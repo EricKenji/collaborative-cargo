996c82a6e25bfda5d79ae85c85734e1b576376c6
ebtaterbug <ityjoseph@gmail.com>
2022-01-12 21:12:09 -0800

    edit user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -88,6 +88,7 @@ router.post('/logout', (req, res) => {
       req.session.destroy(() => {
         res.status(204).end();
       });
+      res.json({ message: 'You are logged out.' });
     }
     else {
       res.status(404).end();
