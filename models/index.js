b506d0838e3c185febf3730c62c1e67ea9903bfc
Eric Furukawa <eric.furukawa@gmail.com>
2022-01-12 20:31:10 -0800

    added associations to model/index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
 module.exports = { User, Post };
