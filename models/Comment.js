06e287d547fc5d876b1358786c1448ab15b9b2b5
Eric Furukawa <eric.furukawa@gmail.com>
2022-01-15 15:31:26 -0800

    added Comment model

---
+++ models/Comment.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_id: {
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Comment;
