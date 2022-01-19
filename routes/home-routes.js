2aac059c8ee1a344f2b927ab8cf23974dbb350e5
ebtaterbug <ityjoseph@gmail.com>
2022-01-18 18:54:10 -0800

    edit views

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -60,6 +60,48 @@ router.get('/users', (req, res) => {
   });
 });
 
+router.get('/user/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'username'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    // serialize the data
+    const user = dbPostData.get({ plain: true });
+
+    // pass data to template
+    res.render('single-user', { 
+        user,
+        loggedIn: req.session.loggedIn
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
